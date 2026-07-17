bf5b44e026a2ee0f5428b6f1a099f4949cfe2bbf
siredave <siredave1@gmail.com>
2026-07-17 14:20:52 -0400

    functions

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -11,3 +11,18 @@ function calculateArea() {
 }
 
 
+let grocery1
+let grocery2
+let grocery3
+
+function calculateGrocery(){
+   grocery1 = parseFloat(document.getElementById('grocery1').value)
+   grocery2 = parseFloat(document.getElementById('grocery2').value)
+   grocery3 = parseFloat(document.getElementById('grocery3').value)
+
+   let totalGrocery = grocery1 + grocery2 + grocery3
+   document.getElementById('result2').innerText= `The total amount is :$ ${totalGrocery}`
+
+}
+
+
